e050bef56051ce01fac31da26a5ec973dabaaff2
David Schneider <schdav@users.noreply.github.com>
2022-03-19 11:35:06 +0100

    Add today's progress

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Clock
 Minimal clock to use as browser startpage.  
 Inspired by an idea of Jake Knapp and John Zeratsky ([Make Time](https://maketime.blog/)).
+
+Also shows today's progress. Inspired by [Year Progress](https://twitter.com/year_progress).
--- clock.js
+++ clock.js
@@ -6,6 +6,8 @@ function showTime() {
   var minute = date.getMinutes();
   var dayTime = 'AM';
 
+  var elapsed = hour * 60 + minute;
+
   if (hour == 0) {
     hour = 12;
   }
@@ -20,6 +22,15 @@ function showTime() {
 
   var time = hour + ':' + minute + ' ' + dayTime;
   document.getElementById('clock').innerText = time;
+
+  var today = date.toLocaleDateString('en-US');
+  document.getElementById('today').innerText = today;
+
+  var minutes = 1440 - elapsed;
+  document.getElementById('minutes').innerText = minutes + ' minutes left';
+
+  var percent = Math.round((elapsed / 1440) * 100);
+  document.getElementById('percent').innerText = 'ca. ' + percent + ' % elapsed';
 }
 
-showTime();
+setTimeout(showTime, 1000);
